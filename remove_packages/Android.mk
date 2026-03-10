71c2bb117005cf9e28fa48fcf7a37dc2d899eccd
Quince <quinceroms@gmail.com>
2026-03-10 02:06:38 +0100

    remove_packages: Exclude some more packages
    Signed-off-by: Quince <quinceroms@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := Accord YouTube Ornament PersonalSafety FossifyGallery MusicFX GoogleFeedback Maps Drive
+LOCAL_OVERRIDES_PACKAGES := Accord YouTube Ornament PersonalSafety FossifyGallery MusicFX GoogleFeedback Maps Drive Photos NowPlayingPrebuilt ScribePrebuilt_v8.4.773573318
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
