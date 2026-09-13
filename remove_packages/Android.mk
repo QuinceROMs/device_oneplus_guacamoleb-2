e99ba625f3fe88f58bae5f1c3ee8ba0392d253cd
Quince <quinceroms@gmail.com>
2026-09-13 16:21:25 +0200

    guacamoleb: remove_packages: Remove MoseyApp
    Signed-off-by: Quince <quinceroms@gmail.com>

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES := arcore CalculatorGooglePrebuilt_85011599 CalendarGooglePrebuilt FilesPrebuilt GoogleFeedback Photos RecorderPrebuilt_847964105 ScribePrebuilt_v8.7.880674799
+LOCAL_OVERRIDES_PACKAGES := arcore CalculatorGooglePrebuilt_85011599 CalendarGooglePrebuilt FilesPrebuilt GoogleFeedback MoseyApp Photos RecorderPrebuilt_847964105 ScribePrebuilt_v8.7.880674799
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
